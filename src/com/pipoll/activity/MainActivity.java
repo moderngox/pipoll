c9a818335067efd00affec5c34783b3c2f9d2c76
Bulbignz <mark_nikil@hotmail.com>
2015-01-19 18:08:39 +0100

    facebook SDK impl

--- src/com/pipoll/activity/MainActivity.java
+++ src/com/pipoll/activity/MainActivity.java
@@ -1,47 +1,67 @@
 package com.pipoll.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.facebook.AppEventsLogger;
 import com.pipoll.R;
+import com.pipoll.fragment.LoginFragment;
 
+public class MainActivity extends FragmentActivity {
 
-public class MainActivity extends Activity {
+	private LoginFragment loginFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+			loginFragment = new LoginFragment();
+			getSupportFragmentManager().beginTransaction()
+					.add(android.R.id.content, loginFragment).commit();
+		} else {
+			// Or set the fragment from restored state info
+			loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(
+					android.R.id.content);
+		}
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    @Override
-    protected void onResume() {
-      super.onResume();
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-      // Logs 'install' and 'app activate' App Events.
-      AppEventsLogger.activateApp(this);
-    }
+		// Logs 'install' and 'app activate' App Events.
+		AppEventsLogger.activateApp(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		// Logs 'app deactivate' App Event.
+		AppEventsLogger.deactivateApp(this);
+	}
+
 }
---
+++ src/com/pipoll/fragment/LoginFragment.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.pipoll.fragment;
+
+import java.util.Arrays;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.widget.LoginButton;
+import com.pipoll.R;
+
+/**
+ * @author moderngox
+ * 
+ */
+public class LoginFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.login, container, false);
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		authButton.setFragment(this);
+		authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
+		return view;
+	}
+}
